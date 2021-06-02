c6576208bc84bc0db6df36bbfff573badc1272ae
rmakarovv <82258730+rmakarovv@users.noreply.github.com>
2021-06-02 13:31:46 +0300

    Update Matrix_Algebra.cpp

--- Matrix_Algebra.cpp
+++ Matrix_Algebra.cpp
@@ -2,7 +2,7 @@
  * Implementation of Matrices storage, operations on matrices. 
  * Jacobi, Seidel's method, least square approximation. 
  * Implemented process of solving an equation Ax = b.
- * @author Roman Makarov, github.com/rmakarovv.
+ * @author Roman Makarov, github.com/rmakarovv
  */
 
 #include <iostream>
